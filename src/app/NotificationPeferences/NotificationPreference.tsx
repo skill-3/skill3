d104cb53e7eb98f46ee8cee54bae72707286ddd4
LING YU QIAN <149683178+lingyuqian0301@users.noreply.github.com>
2024-08-17 09:06:38 +0800

    1

--- src/app/NotificationPeferences/NotificationPreference.tsx
+++ src/app/NotificationPeferences/NotificationPreference.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './Header';
-import Sidebar from './Sidebar';
+import Header from '../Header';
+import Sidebar from '../Sidebar';
 import NotificationSettings from './NotificationSettings';
 import ProfileInfo from './ProfileInfo';
 
