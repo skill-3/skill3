eae6576939611d1c8f97b30d0479ba52968b7e8f
fujianmian <144903122+fujianmian@users.noreply.github.com>
2024-08-18 06:25:32 +0800

    small adjustment

--- src/app/Announcement/MainContent.tsx
+++ src/app/Announcement/MainContent.tsx
@@ -11,7 +11,6 @@ const MainContent: React.FC<MainContentProps> = ({ children }) => {
     <main className="flex flex-col ml-5 max-w-full w-[100%] max-md:ml-0 max-md:w-full">
       <div className="flex flex-col self-stretch my-auto w-full max-md:mt-10 max-md:max-w-full">
         <AnnouncementHeader />
-        <AnnouncementList />
         {children}
       </div>
     </main>
--- src/app/component/ProgressBar.tsx
+++ src/app/component/ProgressBar.tsx
@@ -11,7 +11,7 @@ export default function ProgressBar({ percentage, label }: ProgressBarProps) {
       <p className="text-sm font-medium mb-1">{label}</p>
       <div className="w-full bg-sky-600/20 h-6 rounded-full overflow-hidden">
         <div
-          className="bg-sky-400 h-full"
+          className="bg-sky-400 rounded-full h-full"
           style={{ width: `${percentage}%`, minWidth: "20px" }}
         ></div>
       </div>
