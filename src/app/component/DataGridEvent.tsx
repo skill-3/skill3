5930f322a168298d99f494131edee72c55199b61
Qayyum Yazid <purrnama@proton.me>
2024-08-18 12:09:34 +0800

    Update DataGridEvent.tsx

--- src/app/component/DataGridEvent.tsx
+++ src/app/component/DataGridEvent.tsx
@@ -1,23 +1,23 @@
 // components/DataGridView.tsx
 
-import React from 'react';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import React from "react";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 interface DataGridViewProps {
   rows: { id: number; no: number; event: string; achievement: string }[];
 }
 
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  { field: 'no', headerName: 'No', width: 150 },
-  { field: 'event', headerName: 'Event', width: 150 },
-  { field: 'achievement', headerName: 'Achievement', width: 150 },
+  { field: "id", headerName: "ID", width: 90 },
+  { field: "no", headerName: "No", width: 150 },
+  { field: "event", headerName: "Event", width: 150 },
+  { field: "achievement", headerName: "Achievement", width: 150 },
 ];
 
 const DataGridView: React.FC<DataGridViewProps> = ({ rows }) => {
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid rows={rows} columns={columns} pageSize={5} />
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid rows={rows} columns={columns} />
     </div>
   );
 };
